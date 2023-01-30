c60e8f594408edc3d3fb626cdf1ffeb52bbba5d5
evan laplaige <evan.laplaige.el@gmail.com>
2023-01-30 16:30:18 +0100

    Bipède

---
+++ Bipède.py
@@ -0,0 +1,13 @@
+class Bipède:
+
+    def __init__(self, type_bipede):
+        self.nbJambes = 2
+        self.typebipede = type_bipede
+    def getter_nbJambes(self):
+        pass
+
+    def getter_type_bipede(self):
+        pass
+    
+    def marcher(self, energie, point_a, point_b):
+        pass
